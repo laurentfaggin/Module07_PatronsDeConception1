600c0a742b18025f5433aa3091f85b3acace7ba7
laurentfaggin <laurentfaggin@hotmail.fr>
2023-08-07 07:15:21 -0400

    first commit

---
+++ Module07_PatronsDeConception1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module07_PatronsDeConception1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Module07 Patrons de conception 1
